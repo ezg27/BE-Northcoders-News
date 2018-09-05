35913851e078766a0b15475a89b45a27c2408a31
ezg27 <georgehams27@gmail.com>
2018-09-05 14:07:42 +0100

    change var cors to const

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const apiRouter = require('./routes/api');
