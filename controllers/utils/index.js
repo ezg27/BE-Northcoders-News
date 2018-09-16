a6edd16aa1e2a117ce5304505ca1cc03835d0331
ezg27 <georgehams27@gmail.com>
2018-09-16 14:05:04 +0100

    utils: change lets to const

--- controllers/utils/index.js
+++ controllers/utils/index.js
@@ -2,8 +2,8 @@ const { Topic } = require('../../models');
 
 const createNewTopic = (req, res, next) => {
   const capitalizeFirstLetter = str => str[0].toUpperCase() + str.slice(1);
-  let title = capitalizeFirstLetter(req.params.slug);
-  let topicObj = {
+  const title = capitalizeFirstLetter(req.params.slug);
+  const topicObj = {
     title: title,
     slug: req.params.slug
   }
