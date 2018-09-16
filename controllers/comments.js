6e535fa92092027e8f2709803daf70381433c6fd
ezg27 <georgehams27@gmail.com>
2018-09-16 18:00:22 +0100

    comments.js: refactor functions, more readable

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,9 +2,7 @@ const { Comment } = require('../models');
 
 const getComments = (req, res, next) => {
   Comment.find().populate('created_by')
-    .then(comments => {
-      res.status(200).send({ comments });
-    });
+    .then(comments => res.status(200).send({ comments }));
 };
 
 const deleteCommentById = (req, res, next) => {
@@ -18,15 +16,16 @@ const deleteCommentById = (req, res, next) => {
 }
 
 const adjustCommentVoteCountById = (req, res, next) => {
-  if (Object.keys(req.query)[0] !== 'vote') throw { status: 400, msg: '"vote" is the only valid query!' };
-  let update = req.query.vote === 'up' ? { $inc: { votes: 1 } } : { $inc: { votes: -1 } };
+  if (!req.query.hasOwnProperty('vote')) throw { status: 400, msg: '"vote" is the only valid query!' };
+  let update = req.query.vote === 'up' ? 1 : req.query.vote === 'down' ? -1 : null;
   if (!update) throw { status: 400, msg: 'Query value must be either "up" or "down"!' }
-  let obj = { _id: req.params.comment_id };
-  Comment.findByIdAndUpdate(obj._id, update, {new: true}).populate('created_by').then(comment => {
-    if (!comment) throw { status: 404, msg: 'Comment ID does not exist!' };
-    res.status(200).send({ comment });
-  })
-  .catch(next);
+  Comment.findByIdAndUpdate(req.params.comment_id, { $inc: { votes: update } }, { new: true })
+    .populate('created_by')
+    .then(comment => {
+      if (!comment) throw { status: 404, msg: 'Comment ID does not exist!' };
+      res.status(200).send({ comment });
+    })
+    .catch(next);
 }
 
 module.exports = {getComments, deleteCommentById, adjustCommentVoteCountById};
