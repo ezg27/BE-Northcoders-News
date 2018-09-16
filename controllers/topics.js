b271fb17bbd60a49ac3c90376987e5052c698b4d
ezg27 <georgehams27@gmail.com>
2018-09-16 14:19:15 +0100

    topics.js: getArticlesbyTopicSlug refactor promise.all, more readable

--- controllers/topics.js
+++ controllers/topics.js
@@ -2,46 +2,48 @@ const { Topic, Article, Comment } = require('../models');
 const { createNewTopic } = require('./utils');
 
 const getTopics = (req, res, next) => {
-  Topic.find({}, '-__v').populate('')
+  Topic.find({}, '-__v')
+    .populate('')
     .then(topics => res.status(200).send({ topics }))
     .catch(next);
 };
 
 const getArticlesByTopicSlug = (req, res, next) => {
-  Article.find({belongs_to: req.params.slug}, '-__v').populate('created_by').lean()
-  .then(topicArticles => {
-    if (topicArticles.length === 0) throw { status: 404, msg: 'Topic slug does not exist!' };
-    return Promise.all([
-      Comment.find().populate('created_by').lean(),
-      topicArticles
-    ]);
-  })
-  .then(([comments, topicArticles]) => {
-    let articles = topicArticles.map(article => {
-      let artComs = comments.filter(comment => {
-        return comment.belongs_to.toString() === article._id.toString();
-      }).length;
-      article.comments = artComs;
-      return article;
-    });
-    res.status(200).send({ articles });
-  })
-  .catch(next);
+  const topicArticles = Article.find({ belongs_to: req.params.slug }, '-__v')
+    .populate('created_by')
+    .lean();
+  const comments = Comment.find()
+    .populate('created_by')
+    .lean();
+  if (topicArticles.length === 0)
+    throw { status: 404, msg: 'Topic slug does not exist!' };
+  return Promise.all([comments, topicArticles])
+    .then(([comments, topicArticles]) => {
+      let articles = topicArticles.map(article => {
+        let artComs = comments.filter(comment => {
+          return comment.belongs_to.toString() === article._id.toString();
+        }).length;
+        article.comments = artComs;
+        return article;
+      });
+      res.status(200).send({ articles });
+    })
+    .catch(next);
 };
 
 const addArticleByTopicSlug = (req, res, next) => {
   let slug = { slug: req.params.slug };
   Topic.findOne(slug)
     .then(topic => {
-    if (!topic) createNewTopic(req, res, next);
+      if (!topic) createNewTopic(req, res, next);
       let obj = req.body;
       obj.belongs_to = req.params.slug;
-      return Article.create(obj)
+      return Article.create(obj);
     })
     .then(article => {
       res.status(201).send({ article });
     })
     .catch(next);
-}
+};
 
 module.exports = { getTopics, getArticlesByTopicSlug, addArticleByTopicSlug };
