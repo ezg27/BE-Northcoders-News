64dd875ace64a83fd137737af8f9092f124b24cd
ezg27 <georgehams27@gmail.com>
2018-09-16 13:15:26 +0100

    remove unused import from utils + switch str.charat(0) for str[0]

--- controllers/utils/index.js
+++ controllers/utils/index.js
@@ -1,8 +1,8 @@
-const { Topic, Article } = require('../../models');
+const { Topic } = require('../../models');
 
 const createNewTopic = (req, res, next) => {
   function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+    return str[0].toUpperCase() + str.slice(1);
   }
   let title = capitalizeFirstLetter(req.params.slug);
   let topicObj = {
